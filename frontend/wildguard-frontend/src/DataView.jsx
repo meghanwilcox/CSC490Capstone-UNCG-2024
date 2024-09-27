cc82b5cded34ed9471219fa6ad75ffe5b6f3a634
wmharper <wmharper@uncg.edu>
2024-09-27 18:33:41 -0400

    + plots, data view, requested slides, python notebook, csv
    the "_population_plot.html" files are exported using plotly and can be displayed directly on a webpage. They can be embedded to show our data.
    
    The data view has been edited to display the data plots as well as allow navigation between them. Feel free to change up the visuals from here if what i've placed is ugly.
    
    The python notebooks contain the code I used to create the datasets, process, and merge the datasets, which are uploaded in a merged form in the .csv files

--- frontend/wildguard-frontend/src/DataView.jsx
+++ frontend/wildguard-frontend/src/DataView.jsx
@@ -3,19 +3,51 @@ import './styles/DataView.css';
 import Navbar from './Navbar';
 
 const DataView = () => {
+  // list of plot objects with file names and custom labels/titles
+  const plots = [
+    { file: 'buffalo_population_plot.html', label: 'Wild Water Buffalo Population', title: 'Wild Water Buffalo Population Plot' },
+    { file: 'california_condor_population_plot.html', label: 'California Condor Population', title: 'California Condor Population Plot' },
+    { file: 'flyingfox_population_plot.html', label: 'Rodrigues Flying Fox Population', title: 'Rodrigues Flying Fox Population Plot' },
+    { file: 'greenshank_population_plot.html', label: 'Nordmann\'s Greenshank Population', title: 'Nordmann\'s Greenshank Population Plot' },
+    { file: 'guanaco_population_plot.html', label: 'Guanaco Population', title: 'Guanaco Population Plot' },
+    { file: 'hyena_population_plot.html', label: 'Brown Hyena Population', title: 'Brown Hyena Population Plot' },
+    { file: 'ibex_population_plot.html', label: 'Walia Ibex Population', title: 'Walia Ibex Population Plot' },
+    { file: 'nyala_population_plot.html', label: 'Mountain Nyala Population', title: 'Mountain Nyala Population Plot' },
+    { file: 'parrot_population_plot.html', label: 'Yellow-Eared Parrot Population', title: 'Yellow-Eared Parrot Population Plot' },
+    { file: 'platypus_population_plot.html', label: 'Platypus Population', title: 'Platypus Population Plot' },
+    { file: 'snow_leopard_population_plot.html', label: 'Snow Leopard Population', title: 'Snow Leopard Population Plot' },
+    { file: 'tur_population_plot.html', label: 'West Caucasus Tur Population', title: 'West Caucasus Tur Population Plot' }
+  ];
+
+  const [currentPlot, setCurrentPlot] = useState(plots[0]);
 
   return (
     <div className="data-view-page">
-      {/* Header/Banner */}
-      <Navbar/>
+      <Navbar />
 
-      {/* Main Content */}
+      <div className="plot-navigation">
+        {plots.map((plot, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentPlot(plot)}
+            className="plot-button"
+          >
+            {plot.label}
+          </button>
+        ))}
+      </div>
+
       <div className="data-view-content">
-        <h1>Add your content here Billy.</h1>
+        <h1>{currentPlot.title}</h1>
+        <iframe
+          src={`http://localhost:3000/${currentPlot.file}`} 
+          //The above prefix will have to be changed if you are using a different port or if we deploy to a final website
+          title={currentPlot.title}
+          style={{ width: '100%', height: '600px', border: 'none' }}
+        />
       </div>
     </div>
   );
 };
 
 export default DataView;
-
