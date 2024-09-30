4aff65c393dd485b5f03832ac09d303c4ad37b8e
meghan wilcox <meghanwilcox4235@gmail.com>
2024-09-30 11:40:56 -0400

    added 1-100 poaching coordinates

--- frontend/wildguard-frontend/src/poaching_coordinates.js
+++ frontend/wildguard-frontend/src/poaching_coordinates.js
@@ -1,22 +1,506 @@
 const poaching_coordinates = [
+  {
+    latitude: 51.1657,
+    longitude: 10.4515,
+    date: '2016-01-03',
+    species: 'tiger',
+    place: 'Germany'
+  },
+  {
+    latitude: 47.5162,
+    longitude: 14.5501,
+    date: '2016-01-03',
+    species: 'tiger',
+    place: 'Austria'
+  },
+  {
+    latitude: 51.47197643470021,
+    longitude: -0.45373437555567614,
+    date: '2014-06-01',
+    species: 'tiger',
+    place: 'Heathrow International Airport'
+  },
+  {
+    latitude: 55.7558,
+    longitude: 37.6173,
+    date: '2015-01-01',
+    species: 'tiger, amur leopard',
+    place: 'Moscow'
+  },
+  {
+    latitude: 53.3811,
+    longitude: -1.4701,
+    date: '2015-03-01',
+    species: 'tiger',
+    place: 'Sheffield'
+  },
+  {
+    latitude: 43.1161,
+    longitude: 131.8828,
+    date: '2015-10-01',
+    species: 'tiger',
+    place: 'Primorsky Krai'
+  },
+  {
+    latitude: 61.0042,
+    longitude: 69.0019,
+    date: '2015-10-01',
+    species: 'siberian tiger',
+    place: 'Khanty-Mansi Autonomous Okrug'
+  },
+  {
+    latitude: 51.9557,
+    longitude: 1.3502,
+    date: '2012-08-01',
+    species: 'tiger',
+    place: 'Felixstowe Port'
+  },
+  {
+    latitude: 50.1008,
+    longitude: 14.2632,
+    date: '2016-06-26',
+    species: 'tiger',
+    place: 'Prague Airport'
+  },
+  {
+    latitude: 43.4043,
+    longitude: 6.2782,
+    date: '2019-05-01',
+    species: 'white tiger',
+    place: 'Brignoles'
+  },
+  {
+    latitude: 48.9065,
+    longitude: 2.4619,
+    date: '2016-06-18',
+    species: 'tiger',
+    place: 'Noisy-le-Sec, Seine-Saint-Denis'
+  },
+  {
+    latitude: 45.0355,
+    longitude: 38.9753,
+    date: '2019-07-19',
+    species: 'leopard, tiger',
+    place: 'Krasnodar'
+  },
+  {
+    latitude: 53.7893,
+    longitude: -2.2406,
+    date: '2019-09-24',
+    species: 'tiger',
+    place: 'Burnley'
+  },
+  {
+    latitude: 46.6034,
+    longitude: 1.8883,
+    date: '2008-03-12',
+    species: 'tiger',
+    place: 'France'
+  },
+  {
+    latitude: 51.4416,
+    longitude: 5.4697,
+    date: '2014-01-01',
+    species: 'cheetah, tiger',
+    place: 'Eindhoven'
+  },
+  {
+    latitude: 45.0355,
+    longitude: 38.9753,
+    date: '2019-07-01',
+    species: 'leopard, tiger',
+    place: 'Krasnodar'
+  },
+  {
+    latitude: 52.3689,
+    longitude: 83.7692,
+    date: '2019-06-01',
+    species: 'puma, lion, amur tiger',
+    place: 'Altai Krai'
+  },
+  {
+    latitude: 43.1161,
+    longitude: 131.8828,
+    date: '2019-12-01',
+    species: 'tiger',
+    place: 'Primorsky Krai'
+  },
     {
-      latitude: 50.77968742041855,
-      longitude: 9.963551155532997,
-      date: '2016-01-03',
-      species: 'tiger'
+      latitude: 45.2167,
+      longitude: 132.2167,
+      date: '2018-01-30',
+      species: 'tiger',
+      place: 'Lake Khanka'
     },
     {
-      latitude: 47.65879179726324,
-      longitude: 14.57324039206261,
-      date: '2016-01-03',
-      species: 'tiger'
+      latitude: 44.0583,
+      longitude: 132.0191,
+      date: '2020-11-04',
+      species: 'red book amur tiger',
+      place: 'Primorye'
     },
     {
-      latitude: 51.47197643470021,
-      longitude: -0.45373437555567614,
-      date: '2014-06-01',
-      species: 'tiger'
-    }
-  ];
-  
-  export default poaching_coordinates;
+      latitude: 55.1694,
+      longitude: 23.8813,
+      date: '2019-07-01',
+      species: 'tiger',
+      place: 'Lithuania'
+    },
+    {
+      latitude: 48.3386,
+      longitude: 7.8708,
+      date: '2018-12-01',
+      species: 'puma',
+      place: 'Lahr, Germany'
+    },
+    {
+      latitude: 47.5162,
+      longitude: 14.5501,
+      date: '2019-08-01',
+      species: 'tiger',
+      place: 'Austria'
+    },
+    {
+      latitude: 51.8382,
+      longitude: 23.5042,
+      date: '2019-10-01',
+      species: 'tiger',
+      place: 'Koroszczyn, Poland'
+    },
+    {
+      latitude: 38.3452,
+      longitude: -0.4810,
+      date: '2019-02-12',
+      species: 'african lion, bengal tiger',
+      place: 'Alicante, Spain'
+    },
+    {
+      latitude: 50.6292,
+      longitude: 3.0573,
+      date: '2019-09-19',
+      species: 'panther',
+      place: 'Lille, France'
+    },
+    {
+      latitude: 48.8566,
+      longitude: 2.3522,
+      date: '2018-10-24',
+      species: 'lion',
+      place: 'Paris, France'
+    },
+    {
+      latitude: 48.8698,
+      longitude: 2.3074,
+      date: '2018-11-13',
+      species: 'lion',
+      place: 'Champs-Elysees, Paris, France'
+    },
+    {
+      latitude: 40.8986,
+      longitude: 14.2057,
+      date: '2013-12-01',
+      species: 'tiger',
+      place: 'Mugnano, Naples, Italy'
+    },
+    {
+      latitude: 38.1157,
+      longitude: 13.3615,
+      date: '2019-01-17',
+      species: 'tiger',
+      place: 'Palermo, Italy'
+    },
+    {
+      latitude: 38.1487,
+      longitude: 13.1986,
+      date: '2017-03-01',
+      species: 'tiger',
+      place: 'Carini, Palermo, Italy'
+    },
+    {
+      latitude: 49.4396,
+      longitude: 2.1160,
+      date: '2020-12-16',
+      species: 'bengal tiger',
+      place: 'Blacourt, Oise, France'
+    },
+    {
+      latitude: 49.4967,
+      longitude: 0.7975,
+      date: '2020-11-24',
+      species: 'tiger, lion',
+      place: 'Arelaune-en-Seine, France'
+    },
+    {
+      latitude: 49.4944,
+      longitude: 0.1079,
+      date: '2020-10-06',
+      species: 'tiger',
+      place: 'Le Havre, France'
+    },
+    {
+      latitude: 43.4489,
+      longitude: 5.6834,
+      date: '2019-12-31',
+      species: 'lion',
+      place: 'Trets, Bouches-du-Rhône, France'
+    },
+    {
+      latitude: 48.6489,
+      longitude: 2.4970,
+      date: '2015-05-21',
+      species: 'lion, tiger',
+      place: 'Evry-Grégy-sur-Yerres, France'
+    },
+    {
+      latitude: 48.8911,
+      longitude: 2.2406,
+      date: '2016-06-17',
+      species: 'tiger',
+      place: 'Boute-en-Train, France'
+    },
+    
+      {
+        latitude: 40.3496,
+        longitude: 18.1743,
+        date: '2011-01-02',
+        species: 'lion, Siberian tiger',
+        place: 'Puglia, Italy'
+      },
+      {
+        latitude: 48.8566,
+        longitude: 2.3522,
+        date: '2020-05-03',
+        species: 'white tiger cubs',
+        place: 'France'
+      },
+      {
+        latitude: 48.8566,
+        longitude: 2.3522,
+        date: '2020-09-06',
+        species: 'tiger skin',
+        place: 'France'
+      },
+      {
+        latitude: 41.9028,
+        longitude: 12.4964,
+        date: '2019-01-01',
+        species: 'tiger',
+        place: 'Italy'
+      },
+      {
+        latitude: 41.9028,
+        longitude: 12.4964,
+        date: '2020-01-01',
+        species: 'tiger',
+        place: 'Italy'
+      },
+      {
+        latitude: 9.1187,
+        longitude: 119.8702,
+        date: '2013-08-08',
+        species: 'Sunda pangolin, Snake meat, seaweed, dried squid',
+        place: 'Tubbataha Reefs Natural Park, Philippines'
+      },
+      {
+        latitude: 13.9324,
+        longitude: 121.6063,
+        date: '2013-07-28',
+        species: 'Hill myna, Blue-naped parrot, Palawan hornbill, Palawan river otter',
+        place: 'Lucena City, Quezon, Philippines'
+      },
+      {
+        latitude: 14.6898,
+        longitude: 120.4379,
+        date: '2014-05-21',
+        species: 'Malay monitor lizard',
+        place: 'Morong, Bataan, Philippines'
+      },
+      {
+        latitude: 14.5438,
+        longitude: 120.9983,
+        date: '2014-06-05',
+        species: 'Mindanao water monitor, Philippine sailfin lizard, Tarictic hornbill, others',
+        place: 'Pasay City, Philippines'
+      },
+      {
+        latitude: 7.8192,
+        longitude: 123.4376,
+        date: '2014-06-25',
+        species: 'Hawksbill Turtle, Giant Clam, others',
+        place: 'Pagadian City, Zamboanga del Sur, Philippines'
+      },
+      {
+        latitude: 14.2916,
+        longitude: 120.8831,
+        date: '2014-08-01',
+        species: 'Serpent eagle',
+        place: 'General Trias, Cavite, Philippines'
+      },
+      {
+        latitude: 14.6181,
+        longitude: 121.0606,
+        date: '2014-08-15',
+        species: 'African Lovebird, Budgerigar, others',
+        place: 'Quezon City, Philippines'
+      },
+      {
+        latitude: 14.6333,
+        longitude: 120.9667,
+        date: '2014-09-23',
+        species: 'African Grey Parrot, Philippine Cockatoo, others',
+        place: 'Manila North Cemetery, Philippines'
+      },
+      {
+        latitude: 14.1778,
+        longitude: 120.8217,
+        date: '2014-10-05',
+        species: 'Brahminy kite',
+        place: 'Alfonso, Cavite, Philippines'
+      },
+      {
+        latitude: 14.6760,
+        longitude: 121.0437,
+        date: '2014-12-16',
+        species: 'Various reptiles and birds',
+        place: 'Quezon City, Philippines'
+      },
+      {
+        latitude: 14.5794,
+        longitude: 121.0359,
+        date: '2016-04-05',
+        species: 'Sun conure, African Grey parrot, others',
+        place: 'Cainta, Rizal, Philippines'
+      },
+      {
+        latitude: 14.7751,
+        longitude: 120.9482,
+        date: '2016-05-26',
+        species: 'Brahminy Kite, Scops Owl',
+        place: 'Marilao, Bulacan, Philippines'
+      }
+      ,
+        {
+          latitude: 14.5531,
+          longitude: 120.9936,
+          date: '2016-07-05',
+          species: 'Tarictic hornbill, Cream-bellied Fruit-dove, Blue-crowned racquet-tailed parrot, Scops owl, Ball python, Burmese Python, Albino Burmese python, Pit viper, Red-footed Tortoise, Green Iguana, Monitor Lizard, Tarantula',
+          place: "Carmen's Petshop, Stall # 8, New Cartimar Shopping Center, Pasay City"
+        },
+        {
+          latitude: 14.6006,
+          longitude: 120.9823,
+          date: '2017-02-09',
+          species: 'Emperor Scorpion, Chilean Rose Tarantula, Chaco Golden Knee, Trinidad Olive Tarantula, Augacephalus breyeri, Straight Horned Baboon, Rear Horned Baboon, Grey Mustard Baboon, Poecilotheria formosa, Bengal Ornamental, Poecilotheria tigrinawesseli, Brazilian Salmon Pink, Monocentropus balfouri, Pumpkin Patch, Philippine Orange, Mexican Redknee Tarantula, Mexican Fire Legged, Honduran Curly Hair, Mexican Red Rump, Mexican Flame Knee',
+          place: 'Yellow Cab, along Recto Avenue, Manila'
+        },
+        {
+          latitude: 9.1235,
+          longitude: 125.5375,
+          date: '2017-02-21',
+          species: 'Pitcher Plant, Lady Slipper, Pitcher Plant (seedlings)',
+          place: 'Cabadbaran, Agusan del Norte'
+        },
+        {
+          latitude: 10.3104,
+          longitude: 123.8917,
+          date: '2017-05-29',
+          species: 'Wandering whistling duck, Veiled Chameleon, Blue-tongued skink, Indian Ring-necked Parakeet, Green Iguana, Albino Burmese python, Savannah Monitor Lizard, Caiman Crocodile, African Spurred Tortoise',
+          place: 'Calabanit National Highway fronting E. Alegado Public Terminal'
+        },
+        {
+          latitude: 14.5531,
+          longitude: 120.9936,
+          date: '2017-06-25',
+          species: 'Crested serpent eagle',
+          place: 'Genesis Bus Station, Pasay City'
+        },
+        {
+          latitude: 10.3157,
+          longitude: 123.8854,
+          date: '2017-07-14',
+          species: 'Albino Burmese python, Gray\'s Monitor Lizard',
+          place: 'Sitio Tierra Cura, Barangay Carreta, Cebu City'
+        },
+        {
+          latitude: 10.3167,
+          longitude: 123.8854,
+          date: '2017-07-14',
+          species: 'Blue Foot Baboon, Giant Baboon Spider, Magam Tiger Spider, Pumpkin Patch',
+          place: 'Upper San Roque, Bulacao, Pardo, Cebu City'
+        },
+        {
+          latitude: 14.5995,
+          longitude: 120.9842,
+          date: '2017-08-04',
+          species: 'Philippine hanging Parrot/Colasisi, Java sparrow, Crested myna, Spotted dove, Chestnut munia, Red turtle dove, Budgerigar, Zebra dove',
+          place: 'Plaza Miranda, Quiapo, Manila, Evangelista Street, Quiapo, Manila'
+        },
+        {
+          latitude: 16.0325,
+          longitude: 120.4020,
+          date: '2017-08-18',
+          species: 'Albino Burmese python',
+          place: 'Feeder Road, Guiguilonen, Mangaldan, Pangasinan'
+        },
+        {
+          latitude: 13.4826,
+          longitude: 122.2400,
+          date: '2018-08-09',
+          species: 'Bantigue',
+          place: 'Sitio Pinagpandayan, Umiray, Quezon'
+        },
+        {
+          latitude: 14.5086,
+          longitude: 121.0359,
+          date: '2019-02-22',
+          species: 'Veiled Chameleon, Bearded Dragon, Green Iguana',
+          place: 'NAIA Terminal 2, Arrival Area'
+        },
+        {
+          latitude: 14.5353,
+          longitude: 120.9996,
+          date: '2019-04-01',
+          species: 'Tarantula',
+          place: 'Central Mail Exchange Center at Old Domestic Road, Pasay City'
+        },
+        {
+          latitude: 6.9631,
+          longitude: 126.2158,
+          date: '2019-04-08',
+          species: 'Yellow-faced Myna, Large Fig Parrot, Black Lory, Black Palm Cockatoo, Cassowary, Sulphur-crested Cockatoo, Eclectus Parrot, Long Crested Myna, Red and Blue Lory, Red Lory, Coconut Lorikeet, Blythe\'s Hornbill, Pinon\'s Imperial Pigeon, Victoria Crowned-pigeon, Pesquet\'s Parrot, Black-capped Lory, Wallaby, Long-beaked Echidna, Blue-tongued Skink, Rainer Gunther Tree, Black Tree Monitor, Blue-spotted Monitor Lizard',
+          place: 'Sitio Manggihay, Brgy. Bobon, City of Mati, Davao Oriental'
+        },
+        {
+          latitude: 14.6760,
+          longitude: 120.9822,
+          date: '2019-06-15',
+          species: 'Rufous Hornbill, Hill Myna',
+          place: 'AANI Weekend Market, Western B'
+        },
+        {
+          latitude: 14.1237,
+          longitude: 120.9695,
+          date: '2019-06-28',
+          species: 'Palawan pangolin',
+          place: 'Tagaytay City, Cavite'
+        },
+        {
+          latitude: 14.6091,
+          longitude: 121.0487,
+          date: '2019-08-23',
+          species: 'Red-sided Eclectus Parrot, Peregrine Falcon, African Spurred Tortoise, Green Iguana',
+          place: '3068 Sining, Barangay 602 Zone 60, Santa Mesa, Manila'
+        },
+        {
+          latitude: 9.0804,
+          longitude: 126.1983,
+          date: '2019-10-27',
+          species: 'Black Palm Cockatoo, Papuan Hornbill, Sulfur-crested Cockatoo, Eclectus Parrot, Great-billed Parrot, Blue-tongue Skink, Black Lory, Coconut lorikeet, Southern Crown Pigeon, Rainer Guenther\'s Monitor Lizard, D\'albertis Python, Spice Imperial Pigeon, Cassowary, Wallaby',
+          place: 'Barangay Buenavista, Tandag, Surigao del Sur'
+        }
+      
+      
+      
+];
+
+export default poaching_coordinates;
